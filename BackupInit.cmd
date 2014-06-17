668f6c911a2f18bb279cfecde2197b8794fa6625
Maximilian Weigmann <weigmannmh@yahoo.com>
2014-06-17 16:34:49 +0200

    Change BackupInit
    Change BackupInit.cmd and Description file for rename of Debub Template;
    Added zip file for Download

--- BackupInit.cmd
+++ BackupInit.cmd
@@ -12,9 +12,13 @@ set targetroot=%scriptpath%\Server
 
 ren S_Template.cmd NoExecute_S_Template.cmd
 
+ren S_Template_Debug.cmd NoExecute_S_Template_Debug.cmd
+
 For %%n in (S_*.cmd) do call %%n %scriptpath% %logpath% %targetroot% %%n
 
 ren NoExecute_S_Template.cmd S_Template.cmd
+
+ren NoExecute_S_Template_Debug.cmd S_Template_Debug.cmd
 
 set scriptpath=
 
